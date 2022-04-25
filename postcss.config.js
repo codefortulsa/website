189bafc5f55ce832236344c254c5cfd4fe638348
Alec Helmturner <git@alecvision.com>
2022-04-25 15:01:06 -0500

    add tailwind config; add vscode extensions/config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
-module.exports = () => ({
-  plugins: [require("tailwindcss")],
-})
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
