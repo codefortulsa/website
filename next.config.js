13d6df455447edad50e5d84e1b54065d2ec62dba
Alec Helmturner <git@alecvision.com>
2022-06-25 15:16:07 -0500

    enable direct authoring of blogs in src/pages/blog
     - move content/blog/* to src/pages/blog/
     - allow images from GH domains
     - add packages, tailwind config types; update deps
     - add @next/mdx plugin
     - revert @next/mdx lib in favor of unified lib

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ["ts", "tsx", "js", "jsx"],
+  images: {
+    domains: [
+      /* via.placeholder.com domain should be removed for production */
+      "via.placeholder.com",
+      "raw.githubusercontent.com",
+      "user-images.githubusercontent.com",
+    ],
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
