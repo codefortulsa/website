9cb44b4302404a126bca09ba30feb3de455c2836
NicoleJaneway <bnicolebills@gmail.com>
2022-07-02 15:06:36 -0500

    update nav with cft colors

--- src/features/Nav.tsx
+++ src/features/Nav.tsx
@@ -5,22 +5,22 @@ export default function Nav() {
   return (
     <>
       <div className="w-64 h-screen sm:relative bg-gray-900 shadow md:h-full flex-col justify-between hidden sm:flex">
-        <div className="px-8">
-          <div className="flex items-center pt-10">
+        <div>
+          <div className="flex items-center my-6">
             <Image src={sticker} alt="cft logo" />
           </div>
-          <ul className="mt-12">
-            <li className="w-full text-gray-300 hover:text-gray-500 cursor-pointer mb-6">
-              <p className="text-sm text-center ml-2">projects</p>
+          <ul>
+            <li className="w-full font-extrabold text-cft-black hover:text-gray-500 cursor-pointer py-2 bg-cft-white ">
+              <p className="text-sm text-center">projects</p>
             </li>
-            <li className="w-full text-gray-300 hover:text-gray-500 cursor-pointer mb-6">
-              <p className="text-sm text-center ml-2">join us</p>
+            <li className="w-full font-extrabold text-cft-white hover:text-gray-500 cursor-pointer py-2 bg-cft-red">
+              <p className="text-sm text-center">join us</p>
             </li>
-            <li className="w-full text-gray-300 hover:text-gray-500 cursor-pointer mb-6">
-              <p className="text-sm text-center ml-2">events</p>
+            <li className="w-full font-extrabold text-cft-black hover:text-gray-500 cursor-pointer py-2 bg-cft-white">
+              <p className="text-sm text-center">events</p>
             </li>
-            <li className="w-full text-gray-300 hover:text-gray-500 cursor-pointer mb-6">
-              <p className="text-sm text-center ml-2">blog</p>
+            <li className="w-full font-extrabold text-cft-white hover:text-gray-500 cursor-pointer py-2 bg-cft-blue">
+              <p className="text-sm text-center">blog</p>
             </li>
           </ul>
         </div>
