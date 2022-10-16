baa7533182f9740352902fcdb1f672ce56bf3f69
Alec Helmturner <git@alecvision.com>
2022-10-16 21:13:58 +0000

    - recma-next-static-images to fix markdown images  - refactor to use @next/mdx and @mdx-loader  - move deprecated files into folder for reference

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,13 @@
-import "../styles/global.css";
+import '../styles/global.css';
 
-import DefaultLayout from "../features/DefaultLayout";
-
-import type { NextPage } from "next";
-import type { AppProps } from "next/app";
+import DefaultLayout from '../features/DefaultLayout';
+import {MDXProvider} from '@mdx-js/react';
+import components from '../features/blog/components/prose'
+import type {NextPage} from 'next';
+import type {AppProps} from 'next/app';
 
 // Extend the type of the "Component" prop in AppProps below
-type AppPropsWithLayout<
-  TProps = {},
-  TInitialProps = {}
-> = AppProps<TProps> & {
+type AppPropsWithLayout<TProps = {}, TInitialProps = {}> = AppProps<TProps> & {
   Component: NextPageWithLayout<TProps, TInitialProps>;
 };
 
@@ -17,24 +15,28 @@ type AppPropsWithLayout<
 export type NextPageWithLayout<TProps = {}, TInitialProps = {}> = NextPage<
   TProps,
   TInitialProps
-> & { getLayout?: null | ((page: React.ReactElement) => React.ReactNode) };
+> & {getLayout?: null | ((page: React.ReactElement) => React.ReactNode)};
 
 /**
  * @see https://nextjs.org/docs/advanced-features/custom-app
  * @param param0 an object containing the Page Component and any props passed in
  * from the data-fetching method on that page (getStaticProps, getServerSideProps, etc.)
  */
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+function MyApp({Component, pageProps}: AppPropsWithLayout) {
   // If Component.getLayout is undefined, use the DefaultLayout.
   // The layout can be disabled by setting Componenet.getLayout to null
-  const getLayout: NextPageWithLayout["getLayout"] =
+  const getLayout: NextPageWithLayout['getLayout'] =
     Component.getLayout === null
-      ? (page) => page
+      ? page => page
       : Component.getLayout === undefined
-      ? (page) => <DefaultLayout>{page}</DefaultLayout>
+      ? page => <DefaultLayout>{page}</DefaultLayout>
       : Component.getLayout;
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <MDXProvider components={components}>
+      <Component {...pageProps} />
+    </MDXProvider>
+  );
 }
 
 export default MyApp;
