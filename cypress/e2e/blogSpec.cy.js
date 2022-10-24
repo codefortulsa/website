bf539e74339b1a40b333af8a3065908dfd44285e
laylark <laylan@gmail.com>
2022-10-23 22:58:46 -0500

    Create cypress tests for blog spec

---
+++ cypress/e2e/blogSpec.cy.js
@@ -0,0 +1,23 @@
+describe('Blogs', () => {
+  it('index route loads and shows blog card components', () => {
+    cy.visit('http://localhost:3000/blog');
+    cy.get('.post-list-item');
+  });
+
+  it('post route loads and displays a post', () => {
+    cy.visit('http://localhost:3000/blog');
+    cy.get('.post-list-item a').first().click();
+    cy.wait(5000);
+    cy.get('main');
+  });
+
+  it('index next and prev buttons work', () => {
+    cy.visit('http://localhost:3000/blog');
+    cy.get('.post-list-item a').first().click();
+    cy.wait(5000);
+    cy.get('a').contains('next post', {matchCase: false}).click();
+    cy.wait(5000);
+    cy.get('a').contains('previous post', {matchCase: false}).click();
+    cy.get('main');
+  });
+});
