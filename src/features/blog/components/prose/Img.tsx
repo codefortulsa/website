dc3565773c154be6e6082eb7a5e892f5fd2406f3
Alec Helmturner <git@alecvision.com>
2022-10-25 06:29:42 +0000

    update deps to fix build; use stable next/future/image; cleanup;

--- src/features/blog/components/prose/Img.tsx
+++ src/features/blog/components/prose/Img.tsx
@@ -1,4 +1,4 @@
-import Image, { ImageProps } from "next/image";
+import Image, { ImageProps } from "next/future/image";
 /**
  * This relies on the recma-next-static-images plugin in `next.config.js`, since static images break next/image
  * @see https://github.com/vercel/next.js/discussions/19065#discussioncomment-2341463
@@ -8,13 +8,13 @@ const Img = (props: ImageProps) => {
   if (typeof src !== "string") {
     return (
       //@ts-ignore
-      <Image {...restProps} alt={alt ?? title ?? ""} title={title ?? alt ?? ""} {...src}>
+      <Image {...restProps} alt={alt ?? title ?? ""} title={title ?? alt ?? ""} {...src} loading="eager">
         {children}
       </Image>
     )  
   }
   return (
-    <Image  {...restProps} alt={alt ?? ""} title={title ?? ""} src={src}>
+    <Image  {...restProps} alt={alt ?? ""} title={title ?? ""} src={src} loading="eager">
       {children}
     </Image>
   )
