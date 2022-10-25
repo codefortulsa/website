089a98fc7a9858a3d86b57a35d450f1e3b269113
zenlex <erichrkeil@gmail.com>
2022-10-24 19:51:53 -0500

    reduce sleep times in blog test

--- cypress/e2e/blogSpec.cy.js
+++ cypress/e2e/blogSpec.cy.js
@@ -7,17 +7,16 @@ describe('Blogs', () => {
   it('post route loads and displays a post', () => {
     cy.visit('http://localhost:3000/blog');
     cy.get('.post-list-item a').first().click();
-    cy.wait(5000);
     cy.get('main');
   });
 
   it('index next and prev buttons work', () => {
     cy.visit('http://localhost:3000/blog');
     cy.get('.post-list-item a').first().click();
-    cy.wait(5000);
-    cy.get('a').contains('next post', {matchCase: false}).click();
-    cy.wait(5000);
-    cy.get('a').contains('previous post', {matchCase: false}).click();
+    cy.wait(300);
+    cy.get('a').contains('next post', { matchCase: false }).click();
+    cy.wait(300);
+    cy.get('a').contains('previous post', { matchCase: false }).click();
     cy.get('main');
   });
 });
