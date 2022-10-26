04b36d746646ce0bdb8fd724497742e582c1a47b
Alec Helmturner <git@alecvision.com>
2022-10-25 20:21:44 -0500

    skip cypress blog tests; update deps

--- cypress/e2e/blogSpec.cy.js
+++ cypress/e2e/blogSpec.cy.js
@@ -1,16 +1,16 @@
 describe('Blogs', () => {
-  it('index route loads and shows blog card components', () => {
+  it.skip('index route loads and shows blog card components', () => {
     cy.visit('http://localhost:3000/blog');
     cy.get('.post-list-item');
   });
 
-  it('post route loads and displays a post', () => {
+  it.skip('post route loads and displays a post', () => {
     cy.visit('http://localhost:3000/blog');
     cy.get('.post-list-item a').first().click();
     cy.get('main');
   });
 
-  it('index next and prev buttons work', () => {
+  it.skip('index next and prev buttons work', () => {
     cy.visit('http://localhost:3000/blog');
     cy.get('.post-list-item a').first().click();
     cy.wait(300);
@@ -19,4 +19,4 @@ describe('Blogs', () => {
     cy.get('a').contains('previous post', { matchCase: false }).click();
     cy.get('main');
   });
-}).skip()
+})
