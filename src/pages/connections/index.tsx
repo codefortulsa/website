b1bd92e76f6d11d842680c25498de362030ec03a
zenlex <erichrkeil@gmail.com>
2022-10-29 15:26:44 -0500

    refactor connection component

--- src/pages/connections/index.tsx
+++ src/pages/connections/index.tsx
@@ -3,31 +3,45 @@ import slackLogo from "/public/icons/SlackLogo.svg"
 import meetupLogo from "/public/icons/MeetupLogo.svg"
 import twitterLogo from "/public/icons/TwitterLogo.svg"
 import DefaultLayout from "../../features/DefaultLayout"
-import Image from "next/image"
 import { NextPageWithLayout } from "../_app"
+import IconLink from "../../features/connections/components/IconLink"
+
+const links = [
+	{
+		url: "https://slack.techlahoma.org/",
+		icon: slackLogo,
+		label: 'Slack'
+	},
+	{
+		url: "https://www.meetup.com/techlahoma-foundation/events",
+		icon: meetupLogo,
+		label: 'Meetup'
+	},
+	{
+		url: "https://www.twitter.com/codefortulsa/",
+		icon: twitterLogo,
+		label: 'Twitter'
+	},
+	{
+		url: "https://github.com/codefortulsa/",
+		icon: githubLogo,
+		label: 'GitHub'
+	},
+]
 
 const ConnectionsPage: NextPageWithLayout = () => {
 	return (
 		<>
-			{/*// TODO: Refactor links into a component that takes a url, icon, and label as props then style */}
-			<section className="flex flex-col">
-				<a href={"https://slack.techlahoma.org/"} target='blank' className="flex justify-around items-center">
-					<Image src={slackLogo} alt="slack logo" />
-					<h1>Slack</h1>
-				</a>
-				<a href={"https://www.meetup.com/techlahoma-foundation/events"} target='blank' className="flex justify-around items-center">
-					<Image src={meetupLogo} alt="meetup logo" />
-					<h1>Meetup</h1>
-				</a>
-				<a href={"https://www.twitter.com/codefortulsa/"} target='blank' className="flex justify-around items-center">
-					<Image src={twitterLogo} alt="meetup logo" />
-					<h1>Twitter</h1>
-				</a>
-				<a href={"https://github.com/codefortulsa/"} target='blank' className="flex justify-around items-center">
-					<Image src={githubLogo} alt="meetup logo" />
-					<h1>GitHub</h1>
-				</a>
-			</section>
+			{links.map(
+				({ url, icon, label }, index) => (
+					<IconLink
+						key={index}
+						url={url}
+						icon={icon}
+						label={label}
+						reverse={index % 2 == 0}
+					/>
+				))}
 		</>
 	)
 }
