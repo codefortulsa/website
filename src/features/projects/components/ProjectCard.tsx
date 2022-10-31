13757697a1c349ca4bed6818e1e9c44497f8d178
duonglamh1999 <duonglamh1999@gmail.com>
2022-10-30 20:17:32 -0500

    fixed props problem on project card

--- src/features/projects/components/ProjectCard.tsx
+++ src/features/projects/components/ProjectCard.tsx
@@ -1,15 +1,8 @@
+import { Props } from "@mdx-js/react/lib"
 import { ReactElement } from "react"
 import internal from "stream"
 
-
-function ProjectCard(project:Project ) {
-    const {title,status,pm,repo,img,description,changes,links}=project
-  return (
-    <div>ProjectCard</div>
-  )
-}
-
-export interface Project extends ReactElement = {
+export type ProjectCardProps = {
 title: string,
 status: string,
 pm: string,
@@ -19,4 +12,14 @@ description: string,
 changes: string[],
 links: Array<string>,
 }
+
+const  ProjectCard= ({title,status,pm, repo, img, description,changes, links}:ProjectCardProps) => {
+
+  return (
+    <h1>title</h1>
+    
+  )
+}
+
+
 export default ProjectCard
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,13 +1,48 @@
 import { type } from "os"
 import { title } from "process"
 import DefaultLayout from "../../features/DefaultLayout"
-import ProjectCard, { Project } from "../../features/projects/components/ProjectCard"
+import ProjectCard, {ProjectCardProps} from "../../features/projects/components/ProjectCard"
 import { NextPageWithLayout } from "../_app"
 
-const ProjectPage: NextPageWithLayout = (projects:Array<Project>) => {
-    return <>{projects.map((project,index) => (
-        <ProjectCard project={project} key={index}/>
-    ))} </>
+// export type ProjectCardProps = {
+//     title: string,
+//     status: string,
+//     pm: string,
+//     repo: string,  
+//     img: string, 
+//     description: string,
+//     changes: string[],
+//     links: Array<string>,
+//     }
+const projects:ProjectCardProps[] =
+[
+    {
+        title: 'this is title',
+        status: 'finished',
+        pm: 'John Doe',
+        repo: 'link.com',
+        img: 'https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image-300x225.png', 
+        description: 'this is description bla bla',
+        changes: ['changes1', 'changes2'],
+        links: ['slack.com','github.com']
+    },
+
+]
+
+
+const ProjectPage: NextPageWithLayout = () => {
+    return (
+        <>
+        {
+            projects.map(
+                (projectProps, index)=>{
+                    <ProjectCard
+                    {...projectProps}
+                    key={index}/>
+                }
+            )}
+        </>
+    )
 }
 
 export default ProjectPage
