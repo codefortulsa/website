70e62164f8cee46c36ab4f2900e8483c7ee72ea1
Amar Molinas <aemolinas@gmail.com>
2022-12-18 20:41:53 -0600

    modified:   tailwind.config.js added gradations of cft-red, cft-blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,32 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'cft-red': '#AB3039',
-        'cft-blue': '#133A4E',
+        'cft-red': {
+          50:'#F7EAEB',
+          100:'#7EED6D7',
+          200:'#DDACB0',
+          300:'#CD8388',
+          400:'#BC5961',
+          DEFAULT:'#AB3039',
+          500:'#AB3039',/*cft-red*/
+          600:'#89262E',
+          700:'#671d22',
+          800:'#441317',
+          900:'#220A0B'
+        },
+        'cft-blue': {
+          50:'#E7EBED',
+          100:'#D0D8DC',
+          200:'#a1B0B8',
+          300:'#718995',
+          400:'#426171',
+          DEFAULT:'#133A4E',
+          500:'#133A4E',/*cft-blue*/
+          600:'#0F2E3E',
+          700:'#0B2937',
+          800:'#08171F',
+          900:'#040C10'
+         },
         'cft-black': '#121212',
         'cft-light-gray': '#BEBCBC',
         'cft-white': '#F9F9F9',
