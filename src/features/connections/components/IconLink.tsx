e38e7d7c166dd17da3a956bea8436dadd6317cfa
Amar Molinas <aemolinas@gmail.com>
2023-01-04 22:45:51 -0600

    modified:   src/features/connections/components/IconLink.tsx added hover effects to IconLinks

--- src/features/connections/components/IconLink.tsx
+++ src/features/connections/components/IconLink.tsx
@@ -10,20 +10,26 @@ export type IconLinkProps = {
 const IconLink = ({ url, icon, label, reverse = false }: IconLinkProps) => {
 
 	return (
-		<div className="odd:bg-slate-900 even:bg-slate-800
-		flex even:flex-row sm:even:flex-row-reverse lg:even:flex-row">
+		<div className="flex flex-row bg-cft-black text-cft-white
+			sm:even:flex-row lg:even:flex-row-reverse  
+			  ">
 			<a
 				href={url}
 				target='blank'
 				className="items-center py-5 w-96
 				text-left pl-5 sm:pl-10
-				flex justify-start"
+				flex justify-start
+				hover:transition
+				hover:drop-shadow-[1.75px_1.75px_0.75px_rgba(171,48,57,1.0)] 
+				hover:text-cft-blue-400 
+				hover:scale-105"
 			>
-				<div className="w-32 h-32">
+				<div className="w-32 h-32 ">
 					<Image src={icon} alt="slack logo" />
 				</div>
 
-				<div className="pl-5 sm:pl-10 text-2xl">{label}</div>
+				<div className="pl-5 sm:pl-10 text-2xl"
+				>{label}</div>
 			</a>
 		</div>
 	)
