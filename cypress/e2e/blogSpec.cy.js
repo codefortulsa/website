8e89cad5a72e213a6822ed6c2af2c9eaa405e0f8
Alec Helmturner <git@alecvision.com>
2023-01-23 17:02:24 -0600

    add test

--- cypress/e2e/blogSpec.cy.js
+++ cypress/e2e/blogSpec.cy.js
@@ -11,12 +11,26 @@ describe('Blogs', () => {
   });
 
   it.skip('index next and prev buttons work', () => {
-    cy.visit('http://localhost:3000/blog');
+    cy.visit('http://localhost:3003/blog');
     cy.get('.post-list-item a').first().click();
     cy.wait(300);
-    cy.get('a').contains('next post', { matchCase: false }).click();
+    cy.get('a').contains('next post', {matchCase: false}).click();
     cy.wait(300);
-    cy.get('a').contains('previous post', { matchCase: false }).click();
+    cy.get('a').contains('previous post', {matchCase: false}).click();
     cy.get('main');
   });
-})
+
+  it('blog posts display images properly, if any', () => {
+    cy.visit('http://localhost:3003/blog');
+    cy.get('ul li a').each(link => {
+      cy.visit(`http://localhost:3003/${link.attr('href')}`);
+      cy.wait(1000);
+      cy.get('img').each(($img, index) => {
+        if (index === 0) return; // skip header image
+        cy.wrap($img).as('img');
+        cy.get('@img').should('be.visible');//@ts-expect-error naturalWidth is added at runtime
+        cy.get('@img').and(x => expect(x[0].naturalWidth).to.be.greaterThan(1));
+      });
+    });
+  });
+});
