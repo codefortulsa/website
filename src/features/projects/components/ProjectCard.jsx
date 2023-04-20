c8ce65b37b5f8226fae8cec0b0e8d8ce4f6020a6
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-04-20 10:30:48 -0500

    added component properties

--- src/features/projects/components/ProjectCard.jsx
+++ src/features/projects/components/ProjectCard.jsx
@@ -1,16 +1,13 @@
-const ProjectCard = (props) => {
-    const {
-    title, status
-    }   = props.project
-    
-    return (
-         <div>
-             <h2>{title}</h2>
-            <h3>{status}</h3>
-        </div>
-    )
-}
+const ProjectCard = props => {
+  const {title, status} = props.project;
 
-export default ProjectCard
+  return (
+    <div>
+      <h2>{title}</h2>
+      <h3>{status}</h3>
+    </div>
+  );
+};
+
+export default ProjectCard;
 //this is the actual card template you're working on
-//try to figure out the prettier setup
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,22 +1,27 @@
-import DefaultLayout from "../../features/DefaultLayout"
-import { NextPageWithLayout } from "../_app"
-import ProjectCard from "../../features/projects/components/ProjectCard"
+import DefaultLayout from '../../features/DefaultLayout';
+import {NextPageWithLayout} from '../_app';
+import ProjectCard from '../../features/projects/components/ProjectCard';
 
 const project = {
-    title: "Test Foo Bar",
-    status: "test status",
-
-}
+  title: 'Insert title here',
+  status: 'Insert status here',
+  projectManager: 'Insert manager name',
+  githubLink: 'Insert GitHub link',
+  heroImage: 'Insert hero image',
+  projectDescription: 'Insert project description',
+  projectChanges: 'Insert recent changes',
+  projectLinks: 'Insert project links',
+};
 
 const ProjectsPage = () => {
-	return (
-		<>
-            <ProjectCard project={project} />
-		</>
-	)
-}
+  return (
+    <>
+      <ProjectCard project={project} />
+    </>
+  );
+};
 
-ProjectsPage.getLayout = page => <DefaultLayout>{page}</DefaultLayout>
+ProjectsPage.getLayout = page => <DefaultLayout>{page}</DefaultLayout>;
 
-export default ProjectsPage
+export default ProjectsPage;
 //this is just for passing in data to the card template
