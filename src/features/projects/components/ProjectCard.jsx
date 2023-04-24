947be41647eca0b31f68dd160092e2670f2a9b6a
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-04-24 11:15:00 -0500

    updated return value and action items

--- src/features/projects/components/ProjectCard.jsx
+++ src/features/projects/components/ProjectCard.jsx
@@ -1,22 +1,27 @@
-/* const project = {
-  title: 'Insert title here',
-  status: 'Insert status here',
-  projectManager: 'Insert manager name',
-  githubLink: 'Insert GitHub link',
-  heroImage: 'Insert hero image',
-  projectDescription: 'Insert project description',
-  projectChanges: 'Insert recent changes',
-  projectLinks: 'Insert project links',
-};
+/* Action items:
+    1. Figure out why second return statement is grayed out
+    2. Give each prop the correct tags
+    3. Make sure each prop produces correct output
+    4. Style output based on figma file
 */
 
 const ProjectCard = props => {
-  const {title, status} = props.project;
+  const {title, status, projectManager, githubLink, heroImage, projectDescription, projectChanges, projectLinks} = props.project;
 
   return (
     <div>
       <h2>{title}</h2>
       <h3>{status}</h3>
+      <h3>{projectManager}</h3>
+      <h3>{githubLink}</h3>
+      <h3>{heroImage}</h3>
+    </div>
+  );
+  return (
+    <div>
+      <p>{projectDescription}</p>
+      <ul>{projectChanges}</ul>
+      <p>{projectLinks}</p>
     </div>
   );
 };
