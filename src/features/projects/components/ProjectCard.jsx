354ef211f75e58c683000da77a6eb68a8c883158
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-04-26 08:21:35 -0500

    fixed return statement. section content

--- src/features/projects/components/ProjectCard.jsx
+++ src/features/projects/components/ProjectCard.jsx
@@ -1,27 +1,40 @@
 /* Action items:
-    1. Figure out why second return statement is grayed out
     2. Give each prop the correct tags
     3. Make sure each prop produces correct output
     4. Style output based on figma file
 */
 
 const ProjectCard = props => {
-  const {title, status, projectManager, githubLink, heroImage, projectDescription, projectChanges, projectLinks} = props.project;
+  const {
+    title,
+    status,
+    projectManager,
+    githubLink,
+    heroImage,
+    imageAltText,
+    projectDescription,
+    projectChanges,
+    projectLinks,
+  } = props.project;
 
   return (
     <div>
-      <h2>{title}</h2>
-      <h3>{status}</h3>
-      <h3>{projectManager}</h3>
-      <h3>{githubLink}</h3>
-      <h3>{heroImage}</h3>
-    </div>
-  );
-  return (
-    <div>
-      <p>{projectDescription}</p>
-      <ul>{projectChanges}</ul>
-      <p>{projectLinks}</p>
+      <section>
+        <h2>{title}</h2>
+        <h3>{status}</h3>
+        <h4>{projectManager}</h4>
+      </section>
+      <section>
+        <a>{githubLink}</a>
+      </section>
+      <section>
+        <image alt={imageAltText}>{heroImage}</image>
+        <p>{projectDescription}</p>
+      </section>
+      <section>
+        <ul>{projectChanges}</ul>
+        <p>{projectLinks}</p>
+      </section>
     </div>
   );
 };
